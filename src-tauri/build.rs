7a221c3fd11a6c33a01a1b57052a0ace880940e6
Dani <daniyalbinsikandar@outlook.com>
2026-04-05 13:21:52 +0500

    👑 Added Admin Privileges & Manifest Fix

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,13 @@
 fn main() {
-    tauri_build::build()
+    let mut attributes = tauri_build::Attributes::new();
+
+    #[cfg(windows)]
+    {
+        attributes = attributes.windows_attributes(
+            tauri_build::WindowsAttributes::new()
+                .app_manifest(include_str!("app-manifest.xml"))
+        );
+    }
+
+    tauri_build::try_build(attributes).expect("failed to build Tauri app");
 }
